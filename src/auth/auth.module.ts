670c04ed16920c4bc9abe2744f7079324cc7caaf
Jose A. Gonzalez <jglez.dev@gmail.com>
2024-08-26 21:10:17 -0500

    Create services auth
    - Implement jwt-strategy
    - Install dependecies passport
    - Install dependecies jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { authConstants } from './auth.constants';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: authConstants.secret,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,11 +9,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: parseInt(configService.get<string>('POSTGRES_PORT'), 10),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true, // Be cautious about using synchronize in production
       }),
